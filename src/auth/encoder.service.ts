448c18e1dd399deb983fa3e89145c18ef6db3242
javierdialf <javiereduardodiaz@unicesar.edu.co>
2025-03-26 18:47:05 -0500

    config env commit

--- src/auth/encoder.service.ts
+++ src/auth/encoder.service.ts
@@ -3,7 +3,7 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class EncoderService {
-    //veriificar si es mejor el salt definido o generado por getsalt
+    //cambiar esto despues
     salt: number = 10;
     public encodePassword(password: string): Promise<string> {
         return bcrypt.hashSync(password, this.salt);
